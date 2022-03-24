deec0d1045a0a967a97b948ef3712af8fbbabc6f
byKriz <cristianesquivelh@hotmail.com>
2022-03-24 08:00:54 -0400

    calculando el circulo

--- js/index.js
+++ js/index.js
@@ -11,3 +11,19 @@ const cacularAreaCuadrado = () => {
     //alert(`El area del cuadrado es: ${area}cm`);
     document.getElementById("resultado-cuadrado").innerHTML = `Area: ${area}cm`;
 }
+
+const cacularPerimetroCirculo = () => {
+    const radio = document.getElementById("radio-circulo").value;
+    let perimetro = 2 * Math.PI * radio;
+    perimetro = perimetro.toFixed(2);
+    //alert(`El perimetro del circulo es: ${perimetro}cm`);
+    document.getElementById("resultado-circulo").innerHTML = `Perímetro: ${perimetro}cm`;
+}
+
+const cacularAreaCirculo = () => {
+    const radio = document.getElementById("radio-circulo").value;
+    let area = Math.PI * radio * radio;
+    area = area.toFixed(2);
+    //alert(`El area del circulo es: ${area}cm`);
+    document.getElementById("resultado-circulo").innerHTML = `Area: ${area}cm`;
+}
